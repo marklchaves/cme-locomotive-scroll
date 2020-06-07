c7d03fe4d39293af2ec5883812c2ed4d3bfb3122
mark l chaves <mark@caughtmyeye.cc>
2020-06-07 18:01:32 +0800

    Finished 0.0.1 code
    Updated README.

--- dist/cme-locomotive-scroll.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  let bodyDataScrollContainer = document.querySelector("body");
-  bodyDataScrollContainer.setAttribute("data-scroll-container", "");
-
-  let mainDataScrollSection = document.querySelector("main");
-  mainDataScrollSection.setAttribute("data-scroll-section", "");
-})();
